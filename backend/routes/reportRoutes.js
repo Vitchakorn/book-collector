135af5e3b68692cc6cb52dcd121153041fdbe35b
Vitchakorn <112546047+Vitchakorn@users.noreply.github.com>
2023-06-07 20:09:04 +0700

    admin

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,10 +1,10 @@
 const express = require ("express");
-const {} = require("../controllers/reportController.js");
+const {getReports, createReport} = require("../controllers/reportController.js");
 const validateToken = require("../middleware/validateTokenHandler.js");
 const router = express.Router();
 
 
 router.use(validateToken);
-router.route('/').get(getReports).post(createReports);
+router.route('/').get(getReports).post(createReport);
 
 module.exports = router;
